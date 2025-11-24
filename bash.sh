b1211add6819792b67533fd1e95c3ba19a783e8e
Alirezafhi <alirezafeshki2017@gmail.com>
2025-11-24 20:00:29 +0330

    new records

---
+++ bash.sh
@@ -0,0 +1,18 @@
+source './logger/logger.sh'
+
+shopt -s expand_aliases
+alias a="ls"
+log_info "info"
+log_warn "warn"
+log_error "error"
+
+
+result="192.168.1.101 GET /app/"
+ 
+
+
+
+var="123ali123"
+echo ${var#*123}
+
+ 
--- test.sh
+++ test.sh
@@ -1,8 +1,65 @@
 #!/bin/bash
+# echo "enter"
+# read  -n 2 -p "enter your password : " -s test
+# echo $test
 
-mkdir testFolder
-touch t.txt
-nano test.tsx
+declare -A user
+user[name]="Alireza"
+user[age]=26
+
+echo ${user[@]}
+
+myFunction(){
+ local myScore=4
+((myScore+=5))
+echo $myScore
+echo $name
+ myArray=("ali" "reza" "akbar")
+myArray[3]="mohammad"
+echo ${myArray[3]}
+
+# echo $PATH
+# echo $HOME
+# echo $PWD
+# echo $USER
+# echo  $SHELL
+
+# echo "Script name: $0"
+# echo "Total args: $#"
+# echo "First arg: $1"
+# echo $$
+
+}
+# myFunction "my first Argument"
+
+deploy(){
+  local IP=$1
+  local port=$2
+  local user=$3
+  echo $port $user"@"$IP 
+  ssh -p $port $user"@"$IP
+}
+# deploy "8.8.8.8" "22" "root"
+
+
+
+tested(){
+select option in satart Stop Exit; do
+  case $REPLY in
+    1) echo "Starting..." ;;
+    2) echo "Stopping..." ;;
+    3) break ;;
+    *) echo "Invalid option" ;;
+  esac
+done
+
+
+case $Score in
+2) echo "number is 2" ;; 
+3) echo "number is 3" ;;
+4) echo "number is $Score" 
+esac
+
 for myPath in '*'; do
   echo $myPath
 done
@@ -21,11 +78,6 @@ done
 echo "enter your score"
 read Score
 
-case $Score in
-2) echo "number is 2" ;; 
-3) echo "number is 3" ;;
-4) echo "number is $Score" 
-esac
 
 name="Alireza"
 echo hello world I am $name
@@ -36,3 +88,4 @@ elif [ $name = "Alireza" ]; then
 else
     echo "you are in else"
 fi
+}
